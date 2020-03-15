f4c19b97cd518e07d54cb34f31bd33a9ee614a3b
Mariana3101 <mariana3101@gmail.com>
2020-03-15 15:52:25 -0300

    ajustes

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -47,24 +47,44 @@ function getUserById(id) {
 const controller = {
     // todos los productos
     index: (req, res) => {
+
         db.Products
             .findAll({
                 include: ['user', 'categories', 'brand', 'colors', 'size']
             })
-            .then(products => {
-                db.Users
-                    .findByPk(
-                        req.session.user.id,
-                    )
-                    .then(usersLogged => {
-                        const isLogged = req.session.user;
-                        if (isLogged)
-                            return res.render('productos/todosLosProductos', { usersLogged, products, isLogged });
 
-                    })
-                    .catch(error => console.log(error));
+        .then(products => {
+
+
+
+                return res.render('productos/todosLosProductos', { products });
+
+
+
+
+                /*
+                        Me parece que esta logica no es necesaria, porque la vista todosLosProductos 
+                        debe ser vista por los usuarios esten logueados  o no 
+                    
+                                        .then(products => {
+
+
+                                                db.Users
+                                                    .findByPk(
+                                                        req.session.user.id,
+                                                    )
+
+                                                .then(usersLogged => {
+                                                    const isLogged = req.session.user;
+                                                    if (isLogged)
+                                                        return res.render('productos/todosLosProductos', { usersLogged, products, isLogged });
+
+                                                })*/
+
             })
+            .catch(error => console.log(error));
     },
+
     // crear producto por GET 
     create: (req, res) => {
 
@@ -134,7 +154,6 @@ const controller = {
     },
     // Detalle Producto
     show: (req, res) => {
-
         db.Products
             .findByPk(
                 req.params.id, {
@@ -148,17 +167,13 @@ const controller = {
                 }
             )
             .then(products => {
-                return res.render('productos/detalleProducto', {
-                    products,
-                    id: products.id,
-
-                });
+                return res.render('productos/detalleProducto', { products, id: products.id });
             })
             .catch(error => console.log(error));
     },
     // Editar producto por GET
     edit: (req, res) => {
-
+        db.brands
         db.Products
             .findByPk(req.params.id)
             .then(products => {
@@ -172,14 +187,21 @@ const controller = {
                             .then(colorsInDb => {
                                 sequelize
                                     .query('SELECT * FROM sizes')
-                                    .then(sizesInDb => {
-                                        return res.render('productos/editar', {
-                                            products,
-                                            categories: categoriesInDB[0],
-                                            colors: colorsInDb[0],
-                                            sizes: sizesInDb[0]
-                                        });
-                                    })
+
+                                .then(sizesInDb => {
+                                    sequelize
+                                        .query('SELECT * FROM brands')
+                                        .then(brandsInDb => {
+                                            return res.render('productos/editar', {
+                                                products,
+                                                categories: categoriesInDB[0],
+                                                colors: colorsInDb[0],
+                                                sizes: sizesInDb[0],
+                                                brands: brandsInDb[0]
+                                            });
+                                        })
+
+                                })
 
                             })
 
@@ -196,10 +218,11 @@ const controller = {
                 name: req.body.name,
                 price: req.body.price,
                 image: req.file.filename,
-                brand_id: req.body.brands,
-                size_id: req.body.size,
-                category_id: req.body.categories,
+                brand_id: req.body.brand_id,
+                size_id: req.body.size_id, // no cambia el talle 
+                category_id: req.body.category_id, // no cambia la categoria 
                 colors_id: req.body.colors_id,
+
 
             }, {
                 where: {
@@ -218,137 +241,137 @@ const controller = {
     // const isLogged = req.session.userId ? true : false;
     //let userLogged = getUserById(req.session.userId);
     /*
-    let idProducto = req.params.id;
+                                let idProducto = req.params.id;
 
-    const todosLosProductos = getProducts();
+                                const todosLosProductos = getProducts();
 
-    let elProducto = todosLosProductos.find(function(unProducto) {
+        elProducto = todosLosProductos.find(function(unProducto) {
         return unProducto.id == idProducto;
-    })
+                                })
 
-    res.render('productos/detalleProducto', {
+        render('productos/detalleProducto', {
         idProducto: idProducto,
         elProducto: elProducto,
         isLogged,
-        userLogged
+                                    userLogged
 
-    });
+                                });
 
 
     const isLogged = req.session.userId ? true : false;
-    let userLogged = getUserById(req.session.userId);
+                                let userLogged = getUserById(req.session.userId);
 
     let productos = getProducts();
-    res.render('productos/cargaProducto', { productos, isLogged, userLogged });
+                                res.render('productos/cargaProducto', { productos, isLogged, userLogged });
 
 
-    cargaProducto: (req, res) => {
+        aProducto: (req, res) => {
         const isLogged = req.session.userId ? true : false;
-        let userLogged = getUserById(req.session.userId);
+                                    let userLogged = getUserById(req.session.userId);
 
-        const contenidoProductosJSON = fs.readFileSync(ubicacionProductosJSON, 'utf-8');
+                                    const contenidoProductosJSON = fs.readFileSync(ubicacionProductosJSON, 'utf-8');
 
-        let arrayDeProductos = [];
+                                    let arrayDeProductos = [];
 
-        if (contenidoProductosJSON != '') {
+            contenidoProductosJSON != '') {
             arrayDeProductos = JSON.parse(contenidoProductosJSON);
-        }
+                                    }
 
-        req.body = {
+            body = {
             id: arrayDeProductos.length + 1,
-            ...req.body
+                                        ...req.body
 
-        };
+                                    };
 
         req.body.creador = 'Producto guardado por equipo Viste';
-        req.body.image = req.file.filename;
+                                    req.body.image = req.file.filename;
 
-        arrayDeProductos.push(req.body);
+                                    arrayDeProductos.push(req.body);
 
         let contenidoAGuardar = JSON.stringify(arrayDeProductos, null, ' ');
-        fs.writeFileSync(ubicacionProductosJSON, contenidoAGuardar);
+                                    fs.writeFileSync(ubicacionProductosJSON, contenidoAGuardar);
 
 
         // res.send("¡Producto guardado!");
-        return res.render('productos/todosLosProductos', { isLogged, userLogged, todosLosProductos })
+                                    return res.render('productos/todosLosProductos', { isLogged, userLogged, todosLosProductos })
 
-    },
+                                },
 
-    carrito: (req, res) => {
+        ito: (req, res) => {
         const isLogged = req.session.userId ? true : false;
         let userLogged = getUserById(req.session.userId);
-        res.render('carrito', { isLogged, userLogged });
+                                    res.render('carrito', { isLogged, userLogged });
 
-    },
+                                },
 
 
-    detalleProducto: (req, res) => {
+        lleProducto: (req, res) => {
         const isLogged = req.session.userId ? true : false;
-        let userLogged = getUserById(req.session.userId);
+                                    let userLogged = getUserById(req.session.userId);
 
-        let idProducto = req.params.id;
+                                    let idProducto = req.params.id;
 
-        const todosLosProductos = getProducts();
+                                    const todosLosProductos = getProducts();
 
-        let elProducto = todosLosProductos.find(function(unProducto) {
+            elProducto = todosLosProductos.find(function(unProducto) {
             return unProducto.id == idProducto;
-        })
+                                    })
 
-        res.render('productos/detalleProducto', {
+           rrender('productos/detalleProducto', {
             idProducto: idProducto,
             elProducto: elProducto,
             isLogged,
-            userLogged
+                                        userLogged
 
         });
     },
-    */
+                                */
 
     /* res.render('productos/todosLosProductos');
 
         const isLogged = req.session.userId ? true : false;
-        let userLogged = getUserById(req.session.userId);
+                                let userLogged = getUserById(req.session.userId);
 
-        const todosLosProductos = getProducts();
+                                const todosLosProductos = getProducts();
 
-        res.render('productos/todosLosProductos', {
+            render('productos/todosLosProductos', {
             pageClass: 'page-product',
             todosLosProductos,
             isLogged,
             userLogged
         });
-    },
+                            },
 
-    editarProducto: (req, res) => {
+     ediarProducto: (req, res) => {
         const isLogged = req.session.userId ? true : false;
-        let userLogged = getUserById(req.session.userId);
+                                let userLogged = getUserById(req.session.userId);
 
-        const todosLosProductos = getProducts();
+                                const todosLosProductos = getProducts();
 
         let idProducto = req.params.id;
-        let elProducto = todosLosProductos.find(function(unProducto) {
+            elProducto = todosLosProductos.find(function(unProducto) {
             return unProducto.id == idProducto;
-        })
+                                })
 
-        res.render('productos/editar', {
+            render('productos/editar', {
             idProducto: idProducto,
             elProducto: elProducto,
             isLogged,
-            userLogged
+                                    userLogged
 
-        });
+                                });
 
-    },
+                            },
 
-    productoEditado: (req, res) => {
+        uctoEditado: (req, res) => {
         const isLogged = req.session.userId ? true : false;
-        let userLogged = getUserById(req.session.userId);
+                                let userLogged = getUserById(req.session.userId);
 
-        let todosLosProductos = getProducts();
+                                let todosLosProductos = getProducts();
 
-        todosLosProductos = todosLosProductos.map(function(unProducto) {
-            if (unProducto.id == req.params.id) {
-                unProducto = {
+           tsLosProductos = todosLosProductos.map(function(unProducto) {
+             if unProducto.id == req.params.id) {
+                    oducto = {
                     categoria: req.body.categoria,
                     talle: req.body.talle,
                     color: req.body.color,
@@ -360,33 +383,34 @@ const controller = {
                 return unProducto;
             }
             return unProducto;
-        })
+                                })
 
-        console.log(todosLosProductos);
+                                console.log(todosLosProductos);
 
 
         let contenidoAGuardar = JSON.stringify(todosLosProductos, null, ' ');
-        fs.writeFileSync(ubicacionProductosJSON, contenidoAGuardar);
+                                fs.writeFileSync(ubicacionProductosJSON, contenidoAGuardar);
 
         return res.redirect('/productos/todosLosProductos');
-    },
+                            },
 
-    borrarProducto: (req, res) => {
+      boarProducto: (req, res) => {
         const isLogged = req.session.userId ? true : false;
-        let userLogged = getUserById(req.session.userId);
+                                let userLogged = getUserById(req.session.userId);
 
         const todosLosProductos = getProducts();
-        let productosSinElQueBorramos = todosLosProductos.filter(function(unProducto) {
+            productosSinElQueBorramos = todosLosProductos.filter(function(unProducto) {
             return unProducto.id != req.params.id;
-        })
+                                })
 
         // return res.send(productosSinElQueBorramos)
         // guardo el array con los productos finales
-        fs.writeFileSync(ubicacionProductosJSON, JSON.stringify(productosSinElQueBorramos, null, ' '));
+                                fs.writeFileSync(ubicacionProductosJSON, JSON.stringify(productosSinElQueBorramos, null, ' '));
 
         return res.redirect('/productos/todosLosProductos');
     },*/
 };
+
 
 module.exports = controller;
 module.exports = controller;
--- src/database/models/Colors.js
+++ src/database/models/Colors.js
@@ -7,14 +7,7 @@ module.exports = (sequelize, dataTypes) => {
         },
         name: dataTypes.STRING,
     });
-    Colors.associate = (models) => {
-        Colors.belongsToMany(models.Products, {
-            as: 'products',
-            through: 'colors_product',
-            foreignKey: 'colors_id',
-            otherKey: 'product_id'
-        });
-    }
+
 
     return Colors;
 }
