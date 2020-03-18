eb59b596b7d19cbd22b9aad69bdd4efe3d526e2b
Alejandra Serrano <alejaserra07@gmail.com>
2020-03-17 22:34:58 -0300

    ajustes

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -8,11 +8,17 @@ function authMiddleware(req, res, next) {
 */
 
 // Authentication and Authorization Middleware
+
+
+
 var authMiddleware = function(req, res, next) {
-    console.log(req.session.user);
+    //console.log(req.session.user);
     if (req.session && req.session.user)
         return next();
     else
         return res.redirect('/');
-};
+}
+
+   
+
 module.exports = authMiddleware;
