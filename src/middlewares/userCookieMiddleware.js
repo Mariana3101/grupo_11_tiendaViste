ab7816cffa9bd943773713e665ef88f8670b5e6c
Alejandra Serrano <alejaserra07@gmail.com>
2020-03-23 17:59:57 -0300

    cookie

--- src/middlewares/userCookieMiddleware.js
+++ src/middlewares/userCookieMiddleware.js
@@ -1,10 +1,25 @@
-function userCookieMiddleware(req, res, next) {
+const db = require('../database/models');
+
+const userCookieMiddleware = async (req, res, next) =>{
     res.locals.isLogged = false;
 
     if (req.session.user) {
-
         res.locals.isLogged = true;
         res.locals.user = req.session.user
+    }
+
+    else if (req.cookies.user_email != undefined){
+        let user = await db.Users.findOne({
+            where: { email: req.cookies.user_email }
+        })
+
+        if (user) {
+            req.session.user = user;
+            res.locals.isLogged = true;
+            res.locals.user = req.session.user
+        } else {
+            res.redirect ('ingresar');
+        }
     }
 
     next();
